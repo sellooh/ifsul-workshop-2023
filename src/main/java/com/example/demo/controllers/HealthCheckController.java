e4a3500f778b751120807c014ccb66330c4bda72
Erik <124899450+ErikihM@users.noreply.github.com>
2023-12-15 09:47:49 -0300

    Update HealthCheckController.java

--- src/main/java/com/example/demo/controllers/HealthCheckController.java
+++ src/main/java/com/example/demo/controllers/HealthCheckController.java
@@ -10,6 +10,6 @@ public class HealthCheckController {
 
     @GetMapping("/healthcheck")
     public ResponseEntity<String> healthCheck() {
-        return new ResponseEntity<>("Application is running", HttpStatus.OK);
+        return new ResponseEntity<>("A aplicação está executando", HttpStatus.OK);
     }
 }
